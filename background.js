5a831caf2f31fcca681e0ab51baf4c9745e825b2
LegeBeker <volkanwelp@gmail.com>
2022-11-18 11:50:56 +0100

    ✨ Use content script instead of service worker

--- background.js
+++
@@ -1,9 +0,0 @@
-(async () => {
-    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    if (!tab.url.includes("chrome://")) {
-        await chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            files: ['src/js/script.js']
-        });
-    }
-})();
--- src/js/script.js
+++
@@ -1,2 +0,0 @@
-document.body.style.backgroundColor = 'red';
-console.log('script.js');
